34e17070bbfee9353080a3ed8cc8515f0814caa6
Greg Hamlin <hamling@mssm.org>
2016-11-29 20:22:32 -0500

    more cleanup of forms

--- server.js
+++ server.js
@@ -1,58 +1,18 @@
 var express = require('express');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-
-var Schema = mongoose.Schema;
-var ObjectId = Schema.Types.ObjectId;
-
-var User = mongoose.model('User', new Schema({
-  id: ObjectId,
-  firstName: String,
-  lastName: String,
-  email: { type: String, unique: true},
-  password: String,
-}));
-
+//var bodyParser = require('body-parser');
 
 var app = express();
 
-// connect to mongo database
-mongoose.connect('mongodb://localhost/myauth');
-
 // middleware
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended:true}));
-
-app.get('/', function(req, res) {
-  res.send("Hello world");
-});
+//app.use(bodyParser.urlencoded({ extended:true}));
 
 app.post('/register.html', function(req, res) {
-  // res.json(req.body);
-  var user = new User({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    password: req.body.password,
-  });
-
-  user.save(function(err) {
-    if (err) {
-      var error = 'Something Bad Happened!  Error code: ' + err.code;
-      if (err.code === 11000) {
-        error = 'That email is already taken (not unique)';
-      }
-      res.send(error);
-    }
-    else {
-      res.send("Registration successful!");
-    }
-  });
-
+  res.send("got a post to register.html");
 });
 
-app.get('/logout', function(req, res) {
-  res.redirect('/');
+app.post('/login.html', function(req, res) {
+  res.send("got a post to register.html");
 });
 
 var server = app.listen(8081, function() {
